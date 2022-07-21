76e861e111939586a05fe64bae5c77f64a2503cf
giulianosalvati <salvati.1871471@studenti.uniroma1.it>
2022-07-21 15:41:49 +0200

    minor fixes

--- scripts/deployERC721.py
+++ scripts/deployERC721.py
@@ -1,8 +1,7 @@
-from lib2to3.pgen2 import token
 from brownie import Contract, DiceNFT, accounts
 from web3 import Web3
 import json
-import requests
+
 
 banco = accounts[0]
 base_URI = "https://ipfs.dweb.link/"
@@ -15,6 +14,10 @@ f.close()
 
 
 def deploy_contract():
+    '''
+    check_deployedERC721 check if there is a DiceNFT yet deployed 
+    else it deploy it so, from the first game run, we've only one DiceNft contract
+    '''
     erc721 = DiceNFT.deploy({"from": banco})
     return erc721
 
@@ -23,25 +26,28 @@ def check_deployedERC721():
     # Check if there is already a deployed contract for the ERC721 token DiceNFT
     if not DiceNFT:
         contract = deploy_contract()
+        mint_nftgame(contract)
     else:
         contract = DiceNFT[-1]
     return contract
 
 def mint_nftgame(contract):
     # function that mint nft in storage on ipfs
-    for i in range(len(ipfs_uri)+1):
+    for i in range(len(ipfs_uri)):
         id_index = ipfs_uri[f'{i+1}']
         print(id_index)
         token_uri= f"https://{id_index}.ipfs.dweb.link/"
         print(f'Your token_uri is: {token_uri}')
-        contract.createDice(f"DiceToken{i}",token_uri, {"from": banco})
+        contract.createDice(f"DiceToken{i+1}",token_uri, {"from": banco})
         
     
 
-## Add check mintedNft
+
      
 
 def main():
 
     contract = check_deployedERC721()
-    mint_nftgame(contract)
+    print(contract.address)
+    print(contract.getOwnerDices(banco))
+    
